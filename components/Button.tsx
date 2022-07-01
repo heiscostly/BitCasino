e5689db3859c4a24d90bfa6a86f4e4181bf17810
heiscostly <ayangcostly@gmail.com>
2022-07-01 10:02:43 +0100

    Pushing changes to loader

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<ButtonProps> = ({ code, loading, onClick, label }) => {
 			disabled={!code || loading}
 			onClick={onClick}
 			className='disabled:bg-[#fd4c24bc] disabled:cursor-not-allowed border-0 outline-none bg-[#fd4b24] p-[10px] text-white rounded-[20px] text-sm mb-10 flex items-center justify-center'>
-			<ClipLoader color='#fff' loading={loading} size={20} />
+			<ClipLoader color='#000' loading={loading} size={18} />
 			{!loading && label}
 		</button>
 	);
